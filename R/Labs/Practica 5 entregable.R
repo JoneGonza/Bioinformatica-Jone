2c65b9bcc36adab2ebf2d395b47cdc52c99d5e18
JoneGonza <jgonzinc@myuax.com>
2025-11-14 10:56:05 +0100

    Add files via upload

---
+++ R/Labs/Practica 5 entregable.R
@@ -0,0 +1,229 @@
+#############################################################################
+#
+# PRACTICA R
+#
+# Expresión diferencial de genes de ratón
+# Microarray de Affymetrix (Affymetrix Murine Genome U74A version 2 MG_U74Av2
+# Origen de los datos: GEO GSE5583 (http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE5583)
+# Publicación: Mol Cell Biol 2006 Nov;26(21):7913-28.  16940178 (http://www.ncbi.nlm.nih.gov/pubmed/16940178)
+#
+# Muestras: 3 Wild Type x 3 Histone deacetylase 1 (HDAC1)
+#
+# R código original (credits): Ahmed Moustafa
+#
+#
+##############################################################################
+
+# Instalar RCurl
+
+if (!requireNamespace("BiocManager"))
+    install.packages("BiocManager")
+BiocManager::install("RCurl")
+a
+# Si esto falla, que seguro lo hace tratar de instalarlo usando el menú, Paquetes, Servidor Spain A Coruña, RCurl
+
+# Cargamos el paquete y los datos
+library(RCurl)
+url = getURL ("http://bit.ly/GSE5583_data", followlocation = TRUE)
+data = as.matrix(read.table (text = url, row.names = 1, header = T))
+
+# Chequeamos las dimensiones de los datos, y vemos las primeras y las últimas filas
+dim(data)
+head(data)
+tail(data)
+
+# Hacemos un primer histograma para explorar los datos
+par(mar = c(4,4,2,1)) #Abajo, izquierda, arriba, derecha
+hist(data, col = "gray", main="GSE5583 - Histogram")
+
+# Transformamos los datos con un logaritmo 
+# ¿Qué pasa si hacemos una transformación logarítima de los datos? ¿Para qué sirve?
+# De esta manera mejoramos la visualización de los dato. Para los datos brutos se usa sin transformar, pero para visualizarlo es más útil
+# El histograma es más visual
+data2 = log2(data)
+hist(data2, col = "red", main="GSE5583 (log2) - Histogram")
+
+
+# Hacemos un boxplot con los datos transformados. ¿Qué significan los parámetros que hemos empleado?
+# ¿Qué es un boxplot?
+#Un boxplot es una representación gráfica que resume la distribución de un conjunto de datos numéricos
+# Main, mediana (linea dentro de la caja)
+# Cuartiles (los bordes de la caja; Q1 = 25%, Q3 = 75%)
+# Rango intercuartilítico (IQR = Q3-Q1; la altura de las cajas)
+# Bigotes (se extienden desde los cuartiles hasta 1.5 x IQR o hasta el valor más extremo dentro de ese rango)
+# Outliers (los puntos que están fuera de los bigotes)
+# Saca un boxplot para cada columna. Con eltítulo GSE5583-boxplots. Las = 2 hace que los nombres salgan en vertical
+boxplot(data2, col=c("blue", "blue", "blue",
+	"orange", "orange", "orange"),
+	main="GSE5583 - boxplots", las=2)
+boxplot(data, col=c("blue", "blue", "blue",
+	"orange", "orange", "orange"),
+	main = "GSE5583 - boxplots", las=2)
+# El boxplot sin transformar no queda bien	
+
+# Hacemos un hierarchical clustering de las muestras basándonos en un coeficiente de correlación ç
+# de los valores de expresión. ¿Es correcta la separación?
+hc = hclust(as.dist(1-cor(data2)))
+plot(hc, main="GSE5583 - Hierarchical Clustering")
+# El clustering es para comprobar que hemos conseguido las muestras bien, con patrones simples
+# Nos dice si hemos cogido muestras con genes similares, agrupa WT en un cluster y KO en otro
+
+#######################################
+# Análisis de Expresión Diferencial 
+#######################################
+# Hay un listado de genes con expresiones KO o WT y vamos a ver qué genes son lo suficientemente distintos como para ver si tienen efecto o un papel importante en su expresión
+
+# Primero separamos las dos condiciones. ¿Qué tipo de datos has generado?
+# Hemos generado una variable WT con 3 variables dentro y una variable KO con sus tres variables
+head(data)
+wt <- data[,1:3]
+ko <- data[,4:6]
+class(wt)
+head(wt)
+head(ko)
+# De esta manera se calcula una matriz de datos
+# Calcula las medias de las muestras para cada condición. Usa apply
+wt.mean = apply(wt, 1, mean)
+ko.mean = apply(ko, 1, mean)
+head(wt.mean)
+head(ko.mean)
+
+# ¿Cuál es la media más alta?
+# Quiero poner el tope de los ejes en el valor más alto. La media más alta es 37460.5
+limit = max(wt.mean, ko.mean)
+limit
+
+# Ahora hacemos un scatter plot (gráfico de dispersión)
+plot(ko.mean ~ wt.mean, xlab = "WT", ylab = "KO",
+	main = "GSE5583 - Scatter", xlim = c(0, limit), ylim = c(0, limit))
+# Añadir una línea diagonal con abline
+# Si la correlación fuese perfecta estarían pegados a la línea roja
+abline(0, 1, col = "red")
+
+# ¿Eres capaz de añadirle un grid?
+# Sí, nos añade un fondo de celdas
+grid()
+#abline(a, b): línea de pendiente b y ordenada en el origen a
+#abline(h=y): línea horizontal
+#abline(v=x): línea vertical
+abline(1, 2, col = "red")     # línea y = 2x + 1
+abline(h = 2, col = "green")  # línea y = 2
+abline(v = 3, col = "violet") # línea x = 3
+
+# Calculamos la diferencia entre las medias de las condiciones
+diff.mean = wt.mean - ko.mean
+
+# Hacemos un histograma de las diferencias de medias
+hist(diff.mean, col = "gray")
+
+# Calculamos la significancia estadística con un t-test.
+# Primero crea una lista vacía para guardar los p-values
+# Segundo crea una lista vacía para guardar las estadísticas del test.
+# OJO que aquí usamos los datos SIN TRANSFORMAR. ¿Por qué? porque para analizar los datos es mejor usar los raw data al ser más preciso
+# ¿Cuántas valores tiene cada muestra?
+#Primero he creado un vector de p values vacio, lo mismo de estadísticas. Con un bucle for para cada gen de la tabla crea un vector x que tenga los tres valores en WT y los tres KO
+nrow(data) #número de genes = valores por muestra, 12488
+ncol(data) #número de muestras, 6
+pvalue = NULL 
+tstat = NULL 
+for(i in 1 : nrow(data)) { #Para cada gen
+	x = wt[i,] # gene wt número i
+	y = ko[i,] # gene ko número i
+	
+	# Hacemos el test y guardamos en una variable
+	t = t.test(x, y)
+	
+	# Añadimos el p-value a la lista (saca los p-values y los añadimos a la variable
+	pvalue[i] = t$p.value
+	# Añadimos las estadísticas a la lista
+	tstat[i] = t$statistic
+}
+# Le muestras los primeros
+head(pvalue)
+
+# Ahora comprobamos que hemos hecho TODOS los cálculos
+# Misma longitud que genes que tenga
+length(pvalue)
+# Al no haber evaluado la normalidad, no podemos saber si tiene una distribución normal
+# Hacemos un histograma de los p-values.
+# ¿Qué pasa si le ponemos con una transformación de -log10? Que transformamos los datos para visualizarlos mejor
+# El primero son los p values brutos y el segundo son los p values transformados.
+hist(pvalue,col="gray")
+hist(-log10(pvalue), col = "gray")
+
+# Hacemos un volcano plot. Aquí podemos meter la diferencia de medias y la significancia estadística
+plot(diff.mean, -log10(pvalue), main = "GSE5583 - Volcano")
+# Volcano plot es un gráfico que representa la diferencia dde media con los p-values transformados. Los puntos son los genes.
+
+# Queremos establecer que el mínimo para considerar una diferencia significativa, es con una diferencia de 2 y un p-value de 0.01
+# ¿Puedes representarlo en el gráfico?
+# Se hace un filtro de media y uno de p-value y el corte de p-value es 0.01
+diff.mean_cutoff = 2
+pvalue_cutoff = 0.01
+abline(v = diff.mean_cutoff, col = "blue", lwd = 3)
+#abline(v = -diff.mean_cutoff, col = "red", lwd = 3)
+abline(h = -log10(pvalue_cutoff), col = "green", lwd = 3)
+#Volcano plot para analizar expresiones de genes, representa en el eje x las diferencias de medias. En el eje de y los p values transformados. Los puntos son genes. Los genes que tengan mims expresion en wild type y ko, estarán cercanos al 0, wild type positivos y ko negativos. La linea del dos es que los genes que pasan el corte de significancia (es significativo a partir de o.o1 aquí representado con el 2) estarán por encima de esa línea
+
+# Ahora buscamos los genes que satisfagan estos criterios
+# Primero hacemos el filtro para la diferencia de medias (fold)
+# Filter nos muestra cuantos genes están expresados
+filter_by_diff.mean = abs(diff.mean) >= diff.mean_cutoff
+dim(data[filter_by_diff.mean, ])
+
+# Ahora el filtro de p-value
+filter_by_pvalue = pvalue <= pvalue_cutoff
+dim(data[filter_by_pvalue, ])
+
+# Ahora las combinamos. ¿Cuántos genes cumplen los dos criterios?
+#426 genes
+filter_combined = filter_by_diff.mean & filter_by_pvalue
+filtered = data[filter_combined,]
+dim(filtered)
+head(filtered)
+
+# Ahora generamos otro volcano plot con los genes seleccionados marcados en rojo
+plot(diff.mean, -log10(pvalue), main = "GSE5583 - Volcano #2")
+points (diff.mean[filter_combined], -log10(pvalue[filter_combined]),col = "red")
+
+# Ahora vamos a marcar los que estarían sobreexpresados (rojo) y reprimidos (azul). ¿Por qué parece que están al revés? La sobreexpresión la asociamos con valores positivos por lógica, aquí es alrevés por la decisión del principio de hacer diferencia de medias. 
+plot(diff.mean, -log10(pvalue), main = "GSE5583 - Volcano #3")
+points (diff.mean[filter_combined & diff.mean < 0],
+	-log10(pvalue[filter_combined & diff.mean < 0]), col = "red")
+points (diff.mean[filter_combined & diff.mean > 0],
+	-log10(pvalue[filter_combined & diff.mean > 0]), col = "blue")
+
+
+# Ahora vamos a generar un mapa. Para ello primero tenemos que hacer un cluster de las columnas y los genes 
+# ¿Qué es cada parámetro que hemos usado dentro de la función heatmap? Los genes diferencialmente expresados, para ver los bloques de expresión
+# ¿Eres capaz de cambiar los colores del heatmap? Pista: usar el argumento col y hcl.colors
+rowv = as.dendrogram(hclust(as.dist(1-cor(t(filtered)))))
+colv = as.dendrogram(hclust(as.dist(1-cor(filtered))))
+heatmap(filtered, Rowv=rowv, Colv=colv, cexCol=0.7,labRow=FALSE)
+
+heatmap(filtered)
+
+
+# Ahora vamos a crear un heatmap más chulo. Para ello necesitamos dos paquetes: gplots y RcolorBrewer
+#if (!requireNamespace("BiocManager"))
+#    install.packages("BiocManager")
+#BiocManager::install(c("gplots","RColorBrewer"))
+install.packages("gplots")		
+install.packages("RColorBrewer")	
+
+library(gplots)
+library(RColorBrewer)
+
+# Hacemos nuestro heatmap
+heatmap.2(filtered, Rowv=rowv, Colv=colv, cexCol=0.7,
+	col = rev(redblue(256)), scale = "row")
+# Los rojo es sobreexpersión y lo azul reprimido
+# Lo guardamos en un archivo PDF
+pdf ("GSE5583_DE_Heatmap.pdf")
+heatmap.2(filtered, Rowv=rowv, Colv=colv, cexCol=0.7,
+	col = rev(redblue(256)), scale = "row",labRow=FALSE)
+dev.off()
+heatmap.2(filtered, Rowv=rowv, Colv=colv, cexCol=0.7,col = redgreen(75), scale = "row",labRow=FALSE)
+
+# Guardamos los genes diferencialmente expresados y filtrados en un fichero
+write.table (filtered, "GSE5583_DE.txt", sep = "\t",quote = FALSE)
